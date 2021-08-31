39227bd26b5bec16b643e294474e3475e8c1bb86
Alex Kim <312akim@gmail.com>
2021-08-31 11:53:26 -0700

    Added cors middleware

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const port = 4000
+const cors = require('cors')
 
 const { graphqlHTTP} = require('express-graphql');
 const schema = require('./Schemas/index.js');
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
